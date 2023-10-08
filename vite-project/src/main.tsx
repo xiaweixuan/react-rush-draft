35cc6d1329212c55992719d6cf7568e12fdf93b3
xiaweixuan <weixuan.xwx@gmail.com>
2023-10-08 23:13:48 +0800

    change vite-project

--- vite-project/src/main.tsx
+++ vite-project/src/main.tsx
@@ -1,10 +1,5 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import React from "react";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const root = ReactDOM.createRoot(document.getElementById("root")!);
+root.render(<div>hello world</div>);
